2b3a3a9e0432c6e72693f5fa9344a4ea4fbeff03
Timur Ammaev <timurlego1@yandex.ru>
2024-01-30 16:00:30 +0300

    Removed illegal locking order

--- ReadWriteMutex.cpp
+++ ReadWriteMutex.cpp
@@ -71,7 +71,7 @@ public:
         
         IsCondVarWaiting.store(true);
 
-        if (ReadCounter > 0) Cv.wait(lk);
+        if (ReadCounter.load() > 0) Cv.wait(lk);
         
         IsCondVarWaiting.store(false);
     }
@@ -83,7 +83,8 @@ public:
     }
 };
 
-thread_local std::size_t LocalThreadLockCounter = 0;
+thread_local std::size_t LocalThreadReadLockCounter = 0;
+thread_local std::size_t LocalThreadWriteLockCounter = 0;
 
 /**
     \brief A class for synchronizing threads
@@ -93,90 +94,43 @@ thread_local std::size_t LocalThreadLockCounter = 0;
     The read lock will ensure that no thread using the write lock gets into the code section until all threads using the read lock are unblocked.
     At the same time, after the write lock, no new threads with a read lock will enter the code section until all threads using the write lock are unblocked.
     Recursiveness allows you to call blocking methods in the same thread multiple times without self-locking.
-    At the same time, it is important to follow the blocking procedure. 3 situations are allowed:
-    1. ReadLock -> ReadLock -> ReadUnlock -> ReadUnlock
-    \code
-    RecursiveReadWriteMutex rrwx;
-    rrwx.ReadLock();
-    rrwx.ReadLock();
-    rrwx.ReadUnlock();
-    rrwx.ReadUnlock();
-    \endcode
-    2. WriteLock -> WriteLock -> WriteUnlock -> WriteUnlock
-    \code
-    RecursiveReadWriteMutex rrwx;
-    rrwx.WriteLock();
-    rrwx.WriteLock();
-    rrwx.WriteUnlock();
-    rrwx.WriteUnlock();
-    \endcode
-    3. WriteLock -> ReadLock -> ReadUnlock -> WriteUnlock
-    \code
-    RecursiveReadWriteMutex rrwx;
-    rrwx.WriteLock();
-    rrwx.ReadLock();
-    rrwx.ReadUnlock();
-    rrwx.WriteUnlock();
-    \endcode
-
-    Situation: ReadLock -> WriteLock -> WriteUnlock -> ReadUnlock
-    \code
-    RecursiveReadWriteMutex rrwx;
-    rrwx.ReadLock();
-    rrwx.WriteLock();
-    rrwx.WriteUnlock();
-    rrwx.ReadUnlock();
-    \endcode
-    It is prohibited because it violates the logic of read and write operations and leads to deadlocking
 */
 class RecursiveReadWriteMutex
 {
 private:
-    std::recursive_mutex WriteMutex, ReadMutex;
-    std::atomic_int ReadCounter;
-    std::atomic_bool IsCondVarWaiting;
-    std::condition_variable Cv;
-public:
-    /// \brief Default constructor
-    RecursiveReadWriteMutex() 
-    { 
-        ReadCounter.store(0);
-        IsCondVarWaiting.store(false);
-    }
+    ReadWriteMutex Rwmx;
 
+public:
+    
     /**
         \brief A method for locking a section of code for reading
 
         Using this method, you can lock the code section for reading, which means that all threads using the read lock will have access to data inside the code section
         but threads using the write lock will wait until all read operations are completed.
+        Note that, in fact, blocking for reading inside writing does not make sense, 
+        since the code section is already locked and therefore nothing will happen inside the function in such a situation.
     */
     void ReadLock()
     {
-    if (LocalThreadLockCounter == 0)
-    {
-        WriteMutex.lock();
-        ReadMutex.lock();
-        ReadCounter.fetch_add(1);
-        ReadMutex.unlock();
-        WriteMutex.unlock();
-    }
-    ++LocalThreadLockCounter;
+        if (LocalThreadWriteLockCounter == 0)
+        {
+            if (LocalThreadReadLockCounter == 0)
+                Rwmx.ReadLock();
+
+            ++LocalThreadReadLockCounter;
+        }
     }
 
     /// \brief A method for unlocking a section of code for reading
     void ReadUnlock()
     {
-        if (LocalThreadLockCounter == 1)
+        if (LocalThreadWriteLockCounter == 0)
         {
-            ReadMutex.lock();
-            ReadCounter.fetch_sub(1);
-
-            if (ReadCounter.load() == 0)
-                while (IsCondVarWaiting.load()) Cv.notify_all();
-
-            ReadMutex.unlock();
+            if (LocalThreadReadLockCounter == 1)
+                Rwmx.ReadUnlock();
+            
+            --LocalThreadReadLockCounter;
         }
-        --LocalThreadLockCounter;
     }
 
     /**
@@ -185,32 +139,39 @@ public:
         This method provides exclusive access to a section of code for a single thread.
         All write operations will be performed sequentially.
         This method takes precedence over the read lock, which means that after calling this method, no new read operations will be started.
+        Note that if the write lock is called inside the read lock, then this will be equivalent to unlocking for reading and then locking for writing.
     */
     void WriteLock()
     {
-        std::mutex mtx;
-        std::unique_lock<std::mutex> lk(mtx);
+        if (LocalThreadWriteLockCounter == 0)
+        {
+            if (LocalThreadReadLockCounter > 0)
+                Rwmx.ReadUnlock();
+            
+            Rwmx.WriteLock();
+        }
 
-        WriteMutex.lock();
-        
-        IsCondVarWaiting.store(true);
-
-        if (ReadCounter > 0) Cv.wait(lk);
-        
-        IsCondVarWaiting.store(false);
+        ++LocalThreadWriteLockCounter;
     }
 
     /// \brief A method for unlocking a section of code for writing
+    /// Note that if the write unlock is called inside the read lock, then this will be equivalent to unlocking for writing and then locking for reading.
     void WriteUnlock()
     {
-        WriteMutex.unlock();
+        if (LocalThreadWriteLockCounter == 1)
+        {
+            Rwmx.WriteUnlock();
+            if (LocalThreadReadLockCounter > 0)
+                Rwmx.ReadLock();
+        }
+        --LocalThreadWriteLockCounter;
     }
 };
 
 // Demo
 #include <vector>
 
-ReadWriteMutex Rwx;
+RecursiveReadWriteMutex Rrwmx;
 std::vector<int> Vec;
 std::condition_variable Cv;
 auto const timeout = std::chrono::steady_clock::now() + std::chrono::milliseconds(500);
@@ -226,10 +187,10 @@ void ReadingFunc()
     for (int i = 0; i < 1000; ++i)
     {
         int summ = 0;
-        Rwx.ReadLock();
+        Rrwmx.ReadLock();
         for (const auto& it : Vec)
             summ += it;
-        Rwx.ReadUnlock();
+        Rrwmx.ReadUnlock();
     }
 }
 
@@ -243,9 +204,11 @@ void WritingFunc()
 
     for (int i = 0; i < 1000; ++i)
     {
-        Rwx.WriteLock();
+        Rrwmx.ReadLock();
+        Rrwmx.WriteLock();
         Vec.emplace_back(i);
-        Rwx.WriteUnlock();
+        Rrwmx.WriteUnlock();
+        Rrwmx.ReadUnlock();
     }
 }
 
@@ -286,15 +249,50 @@ int main()
     rrwx.WriteLock();
     rrwx.WriteUnlock();
     rrwx.WriteUnlock();
-
+    
     rrwx.WriteLock();
     rrwx.ReadLock();
     rrwx.ReadUnlock();
-    rrwx.WriteUnlock();
+    rrwx.WriteUnlock();   
 
-    // Deadlock
-    // rrwx.ReadLock();
-    // rrwx.WriteLock();
-    // rrwx.WriteUnlock();
-    // rrwx.ReadUnlock();
+    rrwx.ReadLock();
+    rrwx.WriteLock();
+    rrwx.WriteUnlock();
+    rrwx.ReadUnlock();
+
+    std::thread th4([&]()
+        {
+            rrwx.ReadLock();
+
+            std::cout << "Th1 sleep1 started" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+            std::cout << "Th1 sleep1 ended" << std::endl;
+
+            rrwx.WriteLock();
+
+            std::cout << "Th1 sleep2 started" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+            std::cout << "Th1 sleep2 ended" << std::endl;
+
+            rrwx.WriteUnlock();
+            rrwx.ReadUnlock();
+        });
+
+    std::thread th5([&]()
+        {
+            std::cout << "Th2 sleep1 started" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+            std::cout << "Th2 sleep1 ended" << std::endl;
+
+            rrwx.WriteLock();
+
+            std::cout << "Th2 sleep2 started" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(20000));
+            std::cout << "Th2 sleep2 ended" << std::endl;
+            
+            rrwx.WriteUnlock();
+        });
+
+    th4.join();
+    th5.join();
 }
